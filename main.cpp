2d305ded3ae524b6d530b64106a13118c45fe207
Emilia <65304376+ComradeChan@users.noreply.github.com>
2022-01-12 20:29:16 -0600

    formatted main.cpp using clang-format -i for readability

--- main.cpp
+++ main.cpp
@@ -3,29 +3,31 @@
 using namespace std;
 
 bool isPrime(int currentNum) {
-	for(int increment = 2; increment < currentNum; increment++){
-		if(currentNum % increment == 0){
-			return false;
-		};
-		if(increment * 2 >= currentNum){return true;};
-	};
-	return true;
+  for (int increment = 2; increment < currentNum; increment++) {
+    if (currentNum % increment == 0) {
+      return false;
+    };
+    if (increment * 2 >= currentNum) {
+      return true;
+    };
+  };
+  return true;
 };
 
-
-int main(){
-	int currentNum;
-	currentNum = 3;
-	while(currentNum <=1000000){
-		if(currentNum % 2 != 0){
-		if(isPrime(currentNum)){
-			cout << currentNum <<" is prime \n"; //SHOULD print out whatever prime number is found
-		}
-		else {
-			cout << currentNum <<" is nonprime \n";
-		};
-		}
-		else {cout << currentNum<<" is even \n";}; // prints if current number is even
-		currentNum++; //increments the number when prime testing statement ends
-	};
+int main() {
+  int currentNum;
+  currentNum = 3;
+  while (currentNum <= 1000000) {
+    if (currentNum % 2 != 0) {
+      if (isPrime(currentNum)) {
+        cout << currentNum << " is prime \n"; // SHOULD print out whatever prime
+                                              // number is found
+      } else {
+        cout << currentNum << " is nonprime \n";
+      };
+    } else {
+      cout << currentNum << " is even \n";
+    };            // prints if current number is even
+    currentNum++; // increments the number when prime testing statement ends
+  };
 };
